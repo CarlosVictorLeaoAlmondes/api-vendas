06f296e75e3893597a69cc1a0c762fd62d2b0c88
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2022-09-28 21:31:10 -0300

    Middleware de Autenticação para Proteção das Rotas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: "ab321bffceb1a2da45d2f59a84af068e",
+        expiresIn: "1d",
+    },
+};
--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,6 +1,7 @@
 import AppError from "@shared/errors/AppError";
 import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
+import authConfig from "@config/auth";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
@@ -30,9 +31,9 @@ class CreateSessionsService {
             throw new AppError("Incorrect email/password combination.", 401);
         }
 
-        const token = sign({}, "ab321bffceb1a2da45d2f59a84af068e", {
+        const token = sign({}, authConfig.jwt.secret, {
             subject: user.id,
-            expiresIn: "1d",
+            expiresIn: authConfig.jwt.expiresIn,
         });
 
         return {
